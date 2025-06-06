cb8929a2b7009099c4fa53f397af434e85f348f3
Jenil Desai <jenildev91@gmail.com>
2025-06-06 17:19:12 +0530

    v1.0.0
    + Added First Buggy Version Of The Contract.

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Yieldup {
+    mapping(address => uint256) public balances;
+
+    constructor() {}
+
+    function stack() public payable {
+        require(msg.value > 0, "Deposit must be greater than 0");
+        balances[msg.sender] += msg.value;
+    }
+
+    function unstack() public {
+        uint256 balance = balances[msg.sender];
+        require(balance > 0, "No balance to withdraw");
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(balance);
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -6,18 +6,30 @@ import "forge-std/Test.sol";
 import "src/Contract.sol";
 
 contract TestContract is Test {
-    Contract c;
+    Yieldup y;
 
     function setUp() public {
-        c = new Contract();
+        y = new Yieldup();
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testStack() public {
+        address user = address(0x123);
+        vm.deal(user, 100 ether);
+        vm.startPrank(user);
+
+        y.stack{value: 10 ether}();
+        assertEq(y.balances(user), 10 ether, "Balance should be 10 ether after stacking");
     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testUnstack() public {
+        address user = address(0x123);
+        vm.deal(user, 100 ether);
+        vm.startPrank(user);
+
+        y.stack{value: 10 ether}();
+        assertEq(y.balances(user), 10 ether, "Balance should be 10 ether after stacking");
+
+        y.unstack();
+        assertEq(y.balances(user), 0, "Balance should be 0 after unstacking");
     }
 }
